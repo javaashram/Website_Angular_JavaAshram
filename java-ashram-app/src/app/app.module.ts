8868946cf977a79d2a7f21d5320cbae07ad04fa1
Rahul Naik <Naik_Ra@India.XoriantCorp.com>
2021-07-21 17:21:57 +0530

    Initial Layout

--- java-ashram-app/src/app/app.module.ts
+++ java-ashram-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidenavComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
